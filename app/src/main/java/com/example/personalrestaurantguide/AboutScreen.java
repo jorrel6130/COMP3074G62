f8f109376c651e2f55fe63dd5ab031b78d76d9a3
Gadise oli <101295074@georgebrown.ca>
2024-12-07 20:29:01 -0800

    renamed from Landing

--- app/src/main/java/com/example/personalrestaurantguide/AboutScreen.java
+++ app/src/main/java/com/example/personalrestaurantguide/AboutScreen.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class Landing extends AppCompatActivity {
+public class AboutScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +23,14 @@ public class Landing extends AppCompatActivity {
         // Set an OnClickListener on the login button
         loginButton.setOnClickListener(v -> {
             // Create an Intent to start the LoginActivity
-            Intent intent = new Intent(Landing.this, Login.class);
+            Intent intent = new Intent(AboutScreen.this, Login.class);
             startActivity(intent);
         });
 
         // Set an OnClickListener on the sign-up button
         signUpButton.setOnClickListener(v -> {
             // Create an Intent to start the SignUpActivity
-            Intent intent = new Intent(Landing.this, SignUp.class);
+            Intent intent = new Intent(AboutScreen.this, SignUp.class);
             startActivity(intent);
         });
     }
